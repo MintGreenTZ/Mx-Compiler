09af086eb547ad92e7ab9b4015e6a4efd2dc95c3
MintGreen <tz2012@126.com>
2020-06-15 12:08:25 +0800

    Add cp ./test.s output.s in bash

--- codegen.bash
+++ codegen.bash
@@ -8,3 +8,5 @@ cd "$(dirname "$0")"
 export CCHK="java -classpath ./lib/antlr-4.7.1-complete.jar:./bin Compiler.Main -codegen > ../local_judge/ravel/build/bin/test.s"
 cat > code.mx   # save everything in stdin to program.txt
 $CCHK
+
+cp ./test.s output.s
